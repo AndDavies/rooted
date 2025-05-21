73fe31ec60e65259aa2a4237ef404c58ca08c282
madvaies <m.andrew.davies@gmail.com>
2025-05-21 18:21:13 +0200

    Added rooted blog CMS v4

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,14 @@ const nextConfig = {
   },
   images: {
     unoptimized: false,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'txlrpvmrvmxniulfcqfu.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/images/**', // Be more specific if all your blog images are in a subfolder
+      },
+    ],
   },
 }
 
